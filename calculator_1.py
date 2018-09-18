7eb88de7c5a4d4812b1f5d699d5422fe2164ad60
Joseph Colca <josephcolca@Josephs-Air.localdomain>
2018-09-18 15:29:39 -0500

    Separated out each operand into different functions, renamed functions to total and use one print line

--- calculator_1.py
+++ calculator_1.py
@@ -1,5 +1,5 @@
 number_1 = float(input("Please enter a number: "))
-operand = input("Please enter an operand (+,-,*,/):")
+operand = input("Please enter an operand (+,-,*,/): ")
 number_2 = float(input("Please enter another number: "))
 
 #def total(number_1,number_2):
@@ -17,27 +17,25 @@ number_2 = float(input("Please enter another number: "))
 #      return result
 #print(total(number_1,number_2))
 
-def add(number_1,number_2):
-    if operand == "+":
+
+if operand == "+":
+    def total(number_1,number_2):
       result = number_1 + number_2
       return result
 
-def sub(number_1,number_2):
-    if operand == "-":
+elif operand == "-":
+    def total(number_1,number_2):
       result = number_1 - number_2
       return result
 
-def mult(number_1,number_2):
-    if operand == "*":
-      result = number_1 - number_2
+elif operand == "*":
+    def total(number_1,number_2):
+      result = number_1 * number_2
       return result
 
-def div(number_1,number_2):
-    if operand == "/":
-      result = number_1 - number_2
+elif operand == "/":
+    def total(number_1,number_2):
+      result = number_1 / number_2
       return result
 
-print (add(number_1,number_2))
-print (sub(number_1,number_2))
-print (mult(number_1,number_2))
-print (div(number_1,number_2))
+print(total(number_1,number_2))
