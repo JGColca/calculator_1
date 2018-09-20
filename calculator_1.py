885d12e35b206a53bd06ab47fa33d609f9478e08
Joseph Colca <josephcolca@Josephs-Air.localdomain>
2018-09-20 11:20:46 -0500

    Added try/except Value error for input conditions

--- calculator_1.py
+++ calculator_1.py
@@ -1,12 +1,27 @@
 
 def promptUserForInput():
+  while True:
+      try:
+          number_1 = float(input("Enter first number: "))
+      except ValueError:
+          print("********* Please enter a number. **********")
+          number_1 = float(input("Enter first number: "))
+      try:
+          operand = input("Enter operand (+,-,*,/): ")
+          while operand not in ["+", "-", "*", "/"]:
+            print("********* Please enter an operand. **********")
+            operand = input("Enter operand (+,-,*,/): ")
+      except ValueError:
+          print("********* Please enter an operand. **********")
 
-    number_1 = float(input("Please enter a number: "))
-    operand = input("Please enter an operand (+,-,*,/): ")
-    number_2 = float(input("Please enter another number: "))
-    return (number_1, operand, number_2)
+      try:
+          number_2 = float(input("Enter second number: "))
+      except ValueError:
+          print("********* Please enter a number. **********")
+          number_2 = float(input("Enter second number: "))
 
-    print(number_1)
+      return (number_1, operand, number_2)
+
 
 def total(number_1, operand, number_2):
     if operand == "+":
@@ -23,7 +38,6 @@ def display_result(number_1, operand, number_2, result):
     print()
     print("**********************")
     print("{0} {1} {2} = {3}".format(number_1,operand,number_2,result))
-    #print(f"{no1} {choice} {no2} = {result}")
     print("**********************")
     print()
 
@@ -33,7 +47,7 @@ display_result(number_1, operand, number_2, result)
 
 while True:
   try:
-      choice = input('Do you wish to continue?  Press Y to continue or any other character to quit:')
+      choice = input('Do you wish to continue?  Press Y to continue or any other character to quit: ')
 
       if (choice != "Y" and choice != "y"):
         break
