d90d250f7671e379aab3fca50b2ff45171b6e7de
Joseph Colca <josephcolca@Josephs-Air.localdomain>
2018-09-20 10:40:07 -0500

    Modified calculator to loop and ask the user to break cycle

--- calculator_1.py
+++ calculator_1.py
@@ -1,41 +1,47 @@
-number_1 = float(input("Please enter a number: "))
-operand = input("Please enter an operand (+,-,*,/): ")
-number_2 = float(input("Please enter another number: "))
 
-#def total(number_1,number_2):
-#    if operand == "+":
-#      result = number_1 + number_2
-#      return result
-#    elif operand == "-":
-#      result = number_1 - number_2
-#      return result
-#    elif operand == "-":
-#      result = number_1 * number_2
-#      return result
-#    elif operand == "/":
-#      result = number_1 / number_2
-#      return result
-#print(total(number_1,number_2))
+def promptUserForInput():
 
+    number_1 = float(input("Please enter a number: "))
+    operand = input("Please enter an operand (+,-,*,/): ")
+    number_2 = float(input("Please enter another number: "))
+    return (number_1, operand, number_2)
 
-if operand == "+":
-    def total(number_1,number_2):
-      result = number_1 + number_2
-      return result
+    print(number_1)
 
-elif operand == "-":
-    def total(number_1,number_2):
-      result = number_1 - number_2
-      return result
+def total(number_1, operand, number_2):
+    if operand == "+":
+        result = number_1 + number_2
+    elif operand == "-":
+        result = number_1 - number_2
+    elif operand == "*":
+        result = number_1 * number_2
+    elif operand == "/":
+        result = number_1 / number_2
+    return result
 
-elif operand == "*":
-    def total(number_1,number_2):
-      result = number_1 * number_2
-      return result
+def display_result(number_1, operand, number_2, result):
+    print()
+    print("**********************")
+    print("{0} {1} {2} = {3}".format(number_1,operand,number_2,result))
+    #print(f"{no1} {choice} {no2} = {result}")
+    print("**********************")
+    print()
 
-elif operand == "/":
-    def total(number_1,number_2):
-      result = number_1 / number_2
-      return result
+(number_1,operand,number_2) = promptUserForInput()
+result = total(number_1, operand, number_2)
+display_result(number_1, operand, number_2, result)
 
-print(total(number_1,number_2))
+while True:
+  try:
+      choice = input('Do you wish to continue?  Press Y to continue or any other character to quit:')
+
+      if (choice != "Y" and choice != "y"):
+        break
+      else:
+        (number_1,operand,number_2) = promptUserForInput()
+        result = total(number_1, operand, number_2)
+        display_result(number_1, operand, number_2, result)
+  except ValueError:
+      print("Try again")
+
+print("Thank you for using calculator.")
